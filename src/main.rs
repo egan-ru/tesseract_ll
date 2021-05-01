d13982f0d133e7554c7d17af288646410ddf2f80
egan <egan.fryazino@gmail.com>
2021-05-01 09:46:51 +0300

    feat(app): one more to go

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ pub type klle_str_t = klle_t<*const str>;
 
 /* nurse is sentinel for test list */
 static mut NURSE : MaybeUninit<klle_str_t> = MaybeUninit::uninit();
+static mut THE_0 : MaybeUninit<klle_str_t> = MaybeUninit::uninit();
+static mut THE_1 : MaybeUninit<klle_str_t> = MaybeUninit::uninit();
+static mut THE_2 : MaybeUninit<klle_str_t> = MaybeUninit::uninit();
+static mut THE_3 : MaybeUninit<klle_str_t> = MaybeUninit::uninit();
+static mut THE_4 : MaybeUninit<klle_str_t> = MaybeUninit::uninit();
+
 
 /**
  * print current node payload
@@ -17,9 +23,8 @@ static mut NURSE : MaybeUninit<klle_str_t> = MaybeUninit::uninit();
 pub unsafe fn dbg_print(node : &mut MaybeUninit<klle_str_t>)
 {
         let nurse_ptr : *const klle_str_t = node.as_mut_ptr();
-        if let Some(str_ref) = (*nurse_ptr).data.as_ref() {
-            println!("{}", str_ref);
-        }
+        let str_ref = &*(*nurse_ptr).data;
+        println!("{}", str_ref);
 }
 
 /**
@@ -33,13 +38,37 @@ pub unsafe fn klle_str_static_init(node : &mut MaybeUninit<klle_str_t>, data : *
     (*node_ptr).init(data);
 }
 
+
+/**
+ * init static uninitialized struct
+ * @param node  - [out] node to init
+ * @param data  - [in] data to init
+ */ 
+pub unsafe fn klle_str_static_addh(node : &mut MaybeUninit<klle_str_t>, toadd : &mut MaybeUninit<klle_str_t>)
+{
+    let node_ptr : *mut klle_str_t = node.as_mut_ptr();
+    let toadd_ptr : *mut klle_str_t = toadd.as_mut_ptr();
+    (*node_ptr).addh(toadd_ptr);
+}
+
 fn main()
 {
     println!("Start");
 
     unsafe {
+        /* init variables */
         klle_str_static_init(&mut NURSE, "nurse!");
+        klle_str_static_init(&mut THE_0, "the_0");
+        klle_str_static_init(&mut THE_1, "the_1");
+        klle_str_static_init(&mut THE_2, "the_2");
+        klle_str_static_init(&mut THE_3, "the_3");
+        klle_str_static_init(&mut THE_4, "the_4");
+
+        /* debug print */
         dbg_print(&mut NURSE);
+
+        /* add all to the tail */
+
     }
 
     //unsafe {
